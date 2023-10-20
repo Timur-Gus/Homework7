3b9399936b2f2948113eb33240856dc00fbfefa1
Timur-Gus <timurka24478@gmail.com>
2023-10-20 11:20:24 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,17 @@ public class Main {
             System.out.println("Месяц " + months + ", сумма накоплений равна " + total + " рублей");
         }
         System.out.println("Всего потребуется месяцев: " + months);
+        System.out.println();
+        System.out.println("Задача 2");
+        System.out.println();
+        int i = 0;
+        while (i < 10){
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(;i >= 1; i--){
+            System.out.print(i + " ");
+        }
     }
 }
