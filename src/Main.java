fe894e2c060116fa01d62a3cfd18190ad495531e
Timur-Gus <timurka24478@gmail.com>
2023-10-20 15:06:20 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         for(;i >= 1; i--){
             System.out.print(i + " ");
         }
-        System.out.println();
+        System.out.println("\n");
         System.out.println("Задача 3");
         System.out.println();
         int population = 12_000_000;
@@ -34,6 +34,19 @@ public class Main {
         for (i = 1; i <= 10; i++){
             population += ((population/1000 * birthRate) - (population/1000 * mortalityRate));
             System.out.println("Год " + i + ", численность населения составляет " + population);
+        }
+        System.out.println();
+        System.out.println("Задача 4");
+        System.out.println();
+        int contribution = 15_000;
+        int percent = 7;
+        int contributionSum = 0;//12_500_000
+        months = 0;
+        while (contributionSum < 12_500_000){
+            months++;
+            contributionSum += contributionSum / 100 * percent;
+            contributionSum += contribution;
+            System.out.println("Месяц " + months + ", сумма накоплений " + contributionSum + " рублей");
         }
     }
 }
